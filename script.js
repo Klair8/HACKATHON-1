310473b95b1ff3a599ef2ae98fefb2d8b0ca4b6d
Claire <121937978+Klair8@users.noreply.github.com>
2023-02-03 13:20:03 +0200

    Spaceship crea

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
-let sectionMons = document.getElementById('monstersContainer')
-
 function newMonsters() {
-  for (let i = 0; i < 24; i++) {
-    let newMons = document.createElement('div')    
+  for (let i = 0; i < 48; i++) {
+    let newMons = document.createElement('div')
+    let sectionMons = document.getElementById('monstersContainer')
     newMons.classList.add('monster')
     sectionMons.appendChild(newMons)
   }
 }
 newMonsters()
 
-console.log();
-const btn = document.getElementsByTagName("button")[0];
+const invaders = document.getElementById("animate");
+
+
+const btn = document.getElementById("button");
 btn.addEventListener("click", myMove);
+btn.addEventListener("click", moveSpaceShip);
+// the start button start both monster and spaceship to move 
 
-let pos = 50
 
-function myMove() {  
+function myMove() {
+  let pos = 0
   setInterval(function () {
     if (pos === 300) {
-      pos++;
-      sectionMons.style.right = pos + 'px';    
+      clearInterval();
     } else {
       pos++;
-      sectionMons.style.left = pos + 'px';
+      invaders.style.left = pos + 'px';
     }
   }, 100)
 }
+
+
+let spaceShip = document.getElementById("spaceShip");
+// ==> for moving by himself 
+ function moveSpaceShip() {
+  let pos = 150
+  let id =
+      setInterval(function() {
+               if (pos === 300) {
+      clearInterval(id);
+            } else {
+                pos++;
+                spaceShip.style.right = pos + "px";
+                spaceShip.style.left = pos + 'px';
+            } 
+          }, 20)
+        };
+
+   
+
+function shooting() {
+  for (let i = 0; i <5 ; i++) {
+  let shooting = document.createElement("div");
+  let spaceShip = document.getElementById("spaceShip");
+  shooting.classList.add('laser')
+  spaceShip.appendChild(shooting); 
+}
+};
+shooting();
+
+add.addEventListener("keydown" (e){
+ if (evt.KeyCode === 38){
+  createListElement();
+ }
+};
+ laser.addEventListener("keypress", addlistAfterKeypress);
 
