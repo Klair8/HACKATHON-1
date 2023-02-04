c6c80019df922b1bd56e0361ce4e1efa87aed3f5
Claire <121937978+Klair8@users.noreply.github.com>
2023-02-05 00:51:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 const container = document.querySelector('.container')
-let currentspaceShipIndex = 188
-let width = 18
+let currentspaceShipIndex = 167
+let width = 16
 let direction = 1
 let invaderId
 let aliensRemoved 
 // const resultdisplay =querySelector(."result")
 
 
-for (let i = 0; i < 198; i++) {
+for (let i = 0; i < 175; i++) {
   const square = document.createElement('div');
   container.appendChild(square)
   square.classList.add(`square${i}`)
@@ -18,8 +18,8 @@ const squares = Array.from(document.querySelectorAll('.container div'))
 
 const alienInvader = [
   0,1,2,3,4,5,6,7,8,9,
-  18,19,20,21,22,23,24,25,26,27,
-  36,37,38,39,40,41,42,43,44,45
+  16,17,18,19,20,21,22,23,24,25,
+  32,33,34,35,36,37,38,39,40,41,
 ]
 
 function newMonsters() {
