4c04776d1f9387af33fe42feaf07e03f652d5075
Claire <121937978+Klair8@users.noreply.github.com>
2023-02-05 02:41:31 +0200

    technical done

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
 const container = document.querySelector('.container')
-let currentspaceShipIndex = 167
-let width = 16
+const resultsDisplay = document.querySelector('.results')
+let currentSpaceshipIndex = 188
+let width = 18
 let direction = 1
 let invaderId
-let aliensRemoved 
-// const resultdisplay =querySelector(."result")
+let goingRight = true
+let aliensRemoved = []
+let results = 0
 
 
-for (let i = 0; i < 175; i++) {
+for (let i = 0; i < 198; i++) {
   const square = document.createElement('div');
   container.appendChild(square)
   square.classList.add(`square${i}`)
@@ -16,58 +18,119 @@ for (let i = 0; i < 175; i++) {
 
 const squares = Array.from(document.querySelectorAll('.container div'))
 
-const alienInvader = [
-  0,1,2,3,4,5,6,7,8,9,
-  16,17,18,19,20,21,22,23,24,25,
-  32,33,34,35,36,37,38,39,40,41,
+const alienInvaders = [
+  0,1,2,3,4,5,6,7,8,
+  18,19,20,21,22,23,24,25,26,
+  36,37,38,39,40,41,42,43,44,
 ]
 
-function newMonsters() {
-  for (let i = 0; i < alienInvader.length; i++) {
-    squares[alienInvader[i]].classList.add('monster')
+function drawMonsters() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    if(!aliensRemoved.includes(i)) {
+    squares[alienInvaders[i]].classList.add('monster')
   }
 }
-newMonsters()
+}
+drawMonsters()
 
-squares[currentspaceShipIndex].classList.add('spaceShip');
+function removeMonsters() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.remove('monster')
+  }
+}
 
-// move spaceship
+squares[currentSpaceshipIndex].classList.add('spaceShip');
+
+//MOVE SPACESHIP
+
 function moveSpaceShip(e) {;
-  squares[currentspaceShipIndex].classList.remove('spaceShip');
+  squares[currentSpaceshipIndex].classList.remove('spaceShip');
   switch(e.key) {
     case 'ArrowLeft':
-      if (currentspaceShipIndex % width !== 0) currentspaceShipIndex -=1
+      if (currentSpaceshipIndex % width !== 0) currentSpaceshipIndex -=1
     break
     case 'ArrowRight':
-    if (currentspaceShipIndex % width < width -1 ) currentspaceShipIndex +=1
+    if (currentSpaceshipIndex % width < width -1 ) currentSpaceshipIndex +=1
     break
-    }
-    squares[currentspaceShipIndex].classList.add('spaceShip')
   }
+  squares[currentSpaceshipIndex].classList.add('spaceShip')
+}
+  document.addEventListener('keydown', moveSpaceShip);
 
+// SHOOTING TIME
 
-    document.addEventListener('keydown', moveSpaceShip);
+function shootingTime(e) {
+let laserId
+let currentLaserIndex = currentSpaceshipIndex
+function moveLaser(){
+   squares[currentLaserIndex].classList.remove('laser');
+   currentLaserIndex -= width
+  squares[currentLaserIndex].classList.add('laser');
 
-// move monster
-const btn = document.getElementsByTagName("button")[0];
-btn.addEventListener("click", myMove);
+if(squares[currentLaserIndex].classList.contains('monster')){
+squares[currentLaserIndex].classList.remove('laser')
+squares[currentLaserIndex].classList.remove('monster')
+squares[currentLaserIndex].classList.add('blast')
 
-squares[currentLastMonster].classList.add('last monster');
+setTimeout(()=>squares[currentLaserIndex].classList.remove('blast'),300)
+clearInterval(laserId)
+
+const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+aliensRemoved.push(alienRemoved)
+results++
+resultsDisplay.innerHTML = results
+console.log(aliensRemoved)
+}
+
+}
+switch(e.key){
+  case'ArrowUp':
+  laserId = setInterval(moveLaser,100)
+  }
+}
+document.addEventListener("keydown", shootingTime)
+
+// MOVE MONSTER
 
 function myMove(){
-  squares[currentspaceShipIndex].classList.remove('last monster');
-  if(currentLastMonster % width !== 0 )currentLastMonster +=1
-  break
-  squares[currentLastMonster].classList.add('last monster')
-}
- setInterval( myMove, 100);
+  const leftEdge = alienInvaders[0] % width === 0
+  const rightEdge = alienInvaders[alienInvaders.lenght - 1] % width === 0
+  removeMonsters()
 
-
-function myMove() {
-    setInterval
-    (alienInvader[0] % width < width -1 ) { alienInvader[0] +=1}
-    }, 100)
-
+if (rightEdge && goingRight) {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    alienInvaders[i] += width +1
+    direction = -1
+    goingRight = false
   }
-  myMove();
+}
 
+if  (leftEdge && !goingRight) {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    alienInvaders[i] += width -1
+    direction = 1
+    goingRight = true
+  }
+}
+for (let i = 0; i < alienInvaders.length; i++) {
+  alienInvaders[i] += direction
+}
+drawMonsters()
+
+if (squares[currentSpaceshipIndex].classList.contains('spaceShip', 'monster')) {
+  resultsDisplay.innerHTML = 'GAME OVER'
+  clearInterval(invaderId)
+}
+
+for (let i = 0; i < alienInvaders.length; i++) {
+  if(alienInvaders[i] > (squares.length)) {
+    resultsDisplay.innerHTML = 'GAME OVER'
+    clearInterval(invaderId)
+  }
+}
+if (aliensRemoved.length === alienInvaders.length) {
+  resultsDisplay.innerHTML = 'YOU WIN'
+  clearInterval(invaderId)
+}
+}
+invaderId = setInterval(myMove, 600)
